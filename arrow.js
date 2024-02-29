bc028cbac4659f3bbd9fc4c13e0acc66523d068b
swathi489 <swathirani489@gmail.com>
2024-02-29 21:12:18 +0530

    function

---
+++ arrow.js
@@ -0,0 +1,22 @@
+//using ES5
+
+// function add(x,y)
+// {
+//     return(x+y)
+// }
+// console.log(add(3,7)) //this is ES5
+
+//using ES6 (Arrow function)
+
+// const sum = (x) => x-3;
+
+// console.log(sum(4))  //ES6 arrow function
+
+//when the function has multiple line
+
+// 
+
+const getUser = () =>({
+    name:'sineka', age:12
+})
+console.log((getUser()));
