0ad413c804a847df86c6628bc60ca928b5bb0338
swathi489 <swathirani489@gmail.com>
2024-02-29 21:15:56 +0530

    es6

---
+++ destructuring.js
@@ -0,0 +1,21 @@
+const user = {
+    firstname: "sdhwdj" , lastname:"jshfue" ,age:12,
+address:{
+    city : "tvm",
+    state:"america"
+}
+}
+
+ const {firstname:fname,lastname, ...bharu}=user;
+ console.log(fname);
+ console.log(bharu)
+//  console.log(info);
+
+//Destructuring array
+
+// const user = ["john","mathew","Rooba"]
+// const [a, ,c] = user;
+
+// console.log(a)
+// console.log(c)
+
